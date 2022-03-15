2e9b4482e02a5777b09e88d7c4234cf955da006e
JoaoMarcos17 <joaomarcoscalacia@hotmail.com>
2022-03-15 11:16:15 +0000

    some modification on app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, redirect, url_for, render_template
 #this a simple website in flask, flask is a python framework
 app = Flask(__name__)
-# this website has three pages 
+# this website has three pages
 @app.route('/')
 def index():
-    return render_template("index.html", content=['rebecca','donkey','joao'])
+    return render_template("index.html")
 @app.route('/tribute')
 def tribute():
     return render_template("tribute.html")
